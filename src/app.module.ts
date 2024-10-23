9a2ac3478ac4e2817d8987e4c3154c57dfc67dfd
Oxayr <uzairmaqsood451@gmail.com>
2024-10-23 17:17:03 +0500

    added config and env and connected db with server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_CONNECTION'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private configService: ConfigService) {}
+
+  onModuleInit() {
+    console.log(
+      'Here is the app module database connection string ====>',
+      this.configService.get<string>('DATABASE_CONNECTION'),
+    );
+  }
+}
